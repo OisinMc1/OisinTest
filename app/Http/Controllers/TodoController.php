17785d5b480a8b3674d00c781b4e21f39a5a8c1e
Oisin Mc Cann <N00171111@iadt.ie>
2019-10-15 13:08:21 +0100

    This commit

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -12,6 +12,10 @@ class TodoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+     public function __construct(){
+        $this->middleware('auth');
+     }
+
     public function index()
     {
         $todos = Todo::orderBy('created_at','desc')->paginate(8);
